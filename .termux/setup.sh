4da68227362c757b7ae0ff31f5b4fc0b1a458a60
Emilien Breton <bricktech2000@gmail.com>
2023-09-25 22:01:22 -0400

    added to termux config

--- .termux/setup.sh
+++ .termux/setup.sh
@@ -1,3 +1,15 @@
+# general
+
+mkdir ~/bin && ln -s ~/.termux/termux-file-editor ~/bin/termux-file-editor
+
+# storage
+
+termux-setup-storage
 ln -s /storage/emulated/0/Sync ~/Sync
-mkdir ~/bin
-ln -s ~/.termux/termux-file-editor ~/bin/termux-file-editor
+
+# font
+
+curl -sL https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.zip -o FiraCode.zip
+unzip FiraCode.zip FiraCodeNerdFont-Regular.ttf -d ~/.termux
+mv ~/.termux/FiraCodeNerdFont-Regular.ttf ~/.termux/font.ttf
+rm FiraCode.zip
