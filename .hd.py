caff9a2480e63175423289ca20c05dcff57b0e4a
Emilien Breton <bricktech2000@gmail.com>
2023-12-18 19:48:00 -0500

    cleaned up `.hd.py` script

--- .hd.py
+++ .hd.py
@@ -3,28 +3,28 @@ import sys
 
 
 def search(root_path, accronym):
-  dirs = next(os.walk(root_path))[1]
-  possible_paths = []
-
   if len(accronym) == 0:
-    possible_paths += [root_path]
+    return [root_path]
 
-  for dir in dirs:
-    for i, (dir_char, acc_char) in enumerate(zip(dir, accronym)):
+  candidate_paths = []
+  (_, directories, _) = next(os.walk(root_path))
+
+  for directory in directories:
+    for i, (dir_char, acc_char) in enumerate(zip(directory, accronym)):
       if dir_char.lower() != acc_char.lower():
         break
 
-      possible_paths += search(os.path.join(root_path, dir), accronym[i + 1:])
+      candidate_paths += search(os.path.join(root_path, directory), accronym[i+1:])
 
-  return possible_paths
+  return candidate_paths
 
 
 if len(sys.argv) == 1:
   print('.')
 
 elif len(sys.argv) == 2:
-  possible_paths = search('.', sys.argv[1]) or ['.']
-  most_recent_path = max(possible_paths, key=os.path.getmtime)
+  candidate_paths = search('.', sys.argv[1]) or ['.']
+  most_recent_path = max(candidate_paths, key=os.path.getmtime)
   print(most_recent_path)
 
 else:
